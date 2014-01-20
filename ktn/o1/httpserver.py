ee7a22847913f2b4b1171c958a952d1f1a1c377a
Kristoffer Dalby <kradalby@kradalby.no>
2014-01-20 21:13:44 +0100

    stuff not workin

--- ktn/o1/httpserver.py
+++ ktn/o1/httpserver.py
@@ -23,36 +23,54 @@ while True:
     
     print("Connection opened by: ", addr)
 
-    #Fill in start
-    #Fill in end
-
     try:
-        message = connectionSocket.recv(4096)
+        message = connectionSocket.recv(1024)
         print(message)
         filename = message.split()[1]
         print(filename)
-        try:
-            f = open(filename[1:])
-            outputdata = f.read() 
-        except FileNotFoundError as e:
-            connectionSocket.send(u'HTTP/1.x 404 Not Found')
-            connectionSocket.send(u'Content-Type: text/html; charset=UTF-8')
-            connectionSocket.send(u'Connection: close')
+
+        if filename == '/':
+            filename = 'index.html'
+        else:
+            filename = filename[1:]
+
+        f = open(filename)
+        outputdata = f.read() 
+        print(outputdata)
+
+        connectionSocket.send('HTTP/1.1 200 OK')
+        connectionSocket.send('Content-Type: text/html; charset=UTF-8')
+        connectionSocket.send('Connection: close')
+        connectionSocket.send(outputdata)
+
+        #for i in range(0, len(outputdata)): 
+        #    connectionSocket.send(bytes(outputdata[i], 'utf-8'))
+
+        print("200 sent")
 
             
-
-        connectionSocket.send(u'HTTP/1.x 200 OK')
-        connectionSocket.send(u'Content-Type: text/html; charset=UTF-8')
-        connectionSocket.send(u'Connection: close')
-
-        for i in range(0, len(outputdata)): 
-            connectionSocket.send(outputdata[i])
         
-        connectionSocket.close() 
     except IOError as e:
         print(e)
-        #Send response message for file not found
-        #Fill in start #Fill in end
-        #Close client socket #Fill in start #Fill in end
+
+        connectionSocket.send(bytes('HTTP/1.1 500 Internal Server Error', 'utf-8'))
+        connectionSocket.send(bytes('Content-Type: text/html; charset=UTF-8', 'utf-8'))
+        connectionSocket.send(bytes('Connection: close', 'utf-8'))
+        connectionSocket.send(bytes('<h1>500 Internal Server Error</h1>', 'utf-8'))
+        
+        print("500 sent")
+
+
+    except FileNotFoundError as e:
+        print(e)
+
+        connectionSocket.send(bytes('HTTP/1.1 404 Not Found', 'utf-8'))
+        connectionSocket.send(bytes('Content-Type: text/html; charset=UTF-8', 'utf-8'))
+        connectionSocket.send(bytes('Connection: close', 'utf-8'))
+        connectionSocket.send(bytes('<h1>404 file not found</h1>', 'utf-8'))
+        
+        print("404 sent")
+        
+    connectionSocket.close() 
     
 serverSocket.close()
