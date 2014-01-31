bb78a4367d3f25a61a929190644c7760b1076473
Kristoffer Dalby <kradalby@kradalby.no>
2014-01-31 11:09:20 +0100

    der

--- ktn/o2/smtp3.py
+++ ktn/o2/smtp3.py
@@ -3,14 +3,14 @@ import sys
 import ssl
 import base64
 
-msg = "\r\n I love computer networks!"
-endmsg = "\r\n.\r\n"
+msg = b"I love computer networks!\n"
+endmsg = b"\r\n.\r\n"
 
 # Choose a mail server (e.g. Google mail server) and call it mailserver 
 mailserver = "smtp.gmail.com"
 port = 587
-username = "dummy@dfekt.no"
-password = "PASSWORD"
+username = b"dummy@dfekt.no"
+password = b""
 
 # Create socket called clientSocket and establish a TCP connection with mailserver 
 
@@ -24,36 +24,69 @@ print(recv)
 
 if recv[:3] != b'220':
     sys.exit(b"Error here: " + recv)
-# Send HELO command and print server response. 
+else:
+    print(recv)
 
-heloCommand = b'HELO Alice\r\n' 
+
+heloCommand = b'HELO Alice\n' 
+print(heloCommand)
 clientSocket.send(heloCommand)
 
 recv = clientSocket.recv(1024)
 
 if recv[:3] != b'250':
     sys.exit(b"Error here: " + recv)
+else:
+    print(recv)
 
 
-starttls = "STARTTLS\r\n"
+starttls = b"STARTTLS\r\n"
+
+clientSocket.send(starttls)
+
 recv = clientSocket.recv(1024)
 if recv[:3] != b'220':
     sys.exit(b"Error here: " + recv)
+else:
+    print(recv)
 
-clientSocketSecure = ssl.wrap_socket(clientSocket, ssl_version=ssl.PROTOCOL_SSLv23)
+clientSocketSecure = ssl.wrap_socket(clientSocket, ssl_version=ssl.PROTOCOL_TLSv1)
 
 
-clientSocketSecure.send('auth login\n')
+clientSocketSecure.send(b'AUTH LOGIN\n')
 recv = clientSocketSecure.recv(1024)
-print(recv)
+if recv[:3] != b'334':
+    sys.exit(b"Error here: " + recv)
+else:
+    print(recv)
 
-clientSocketSecure.send(base64.b64encode('username')+'\r\n')
-clientSocketSecure.send(base64.b64encode('password')+'\r\n')
+
+uencode = base64.b64encode(username)+b'\r\n'
+pencode = base64.b64encode(password)+b'\r\n'
+
+print(uencode)
+print(pencode)
+
+clientSocketSecure.send(uencode)
+
+recv = clientSocketSecure.recv(1024)
+if recv[:3] != b'334':
+    sys.exit(b"Error here: " + recv)
+else:
+    print(recv)
 
 
+clientSocketSecure.send(pencode)
 
-mailfrom = b"dummy@dfekt.no\n"
-rcptto = b"kradalby@dfektlan.no\n"
+recv = clientSocketSecure.recv(1024)
+if recv[:3] != b'235':
+    sys.exit(b"Error here: " + recv)
+else:
+    print(recv)
+
+
+mailfrom = b"<dummy@dfekt.no>\n"
+rcptto = b"<kradalby@dfektlan.no>\n"
 data = b"DATA\n"
 subject = b"TESTY\n"
 message = b"""HEI DIN TEST
@@ -99,7 +132,8 @@ if recv[:3] != b'354':
 else:
     print(recv)
 
-clientSocket.send(message)
+print(msg)
+clientSocket.send(msg)
 #recv = clientSocket.recv(1024)
 #if recv[:3] != 250:
 #    sys.exit(b"Error here: " + recv)
