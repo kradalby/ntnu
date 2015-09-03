b65dbd41449a37c44282c07fc9a989b6f68d9587
Kristoffer Dalby <kradalby@kradalby.no>
2015-09-03 14:11:59 +0200

    moving to arcanine

--- tdt4200/ps1/computeMPI.c
+++ tdt4200/ps1/computeMPI.c
@@ -4,53 +4,93 @@
 #include <math.h>
 
 /*
-	A simple MPI example.
-	TODO:
-	1. Fill in the needed MPI code to make this run on any number of nodes.
-	2. The answer must match the original serial version.
-	3. Think of corner cases (valid but tricky values).
+    A simple MPI example.
+    TODO:
+    1. Fill in the needed MPI code to make this run on any number of nodes.
+    2. The answer must match the original serial version.
+    3. Think of corner cases (valid but tricky values).
 
-	Example input:
-	./simple 2 10000
+    Example input:
+    ./simple 2 10000
 
 */
 
-int main(int argc, char **argv) {
-	int rank, size;
+double calculate_interval(int start, int stop);
 
-	if (argc < 3) {
-		printf("This program requires two parameters:\n \
+int main(int argc, char **argv) {
+    int rank, size;
+
+    if (argc < 3) {
+        printf("This program requires two parameters:\n \
 the start and end specifying a range of positive integers in which \
 start is 2 or greater, and end is greater than start.\n");
-		exit(1);
-	}
+        exit(1);
+    }
 
-	int start = atoi(argv[1]);
-	int stop = atoi(argv[2]);
-
-
-	if(start < 2 || stop <= start){
-		printf("Start must be greater than 2 and the end must be larger than start.\n");
-		exit(1);
-	}
-
-	// TODO: Compute the local range, so that all the elements are accounted for.
+    int start = atoi(argv[1]);
+    int stop = atoi(argv[2]);
 
 
-	// Perform the computation
-	double sum = 0.0;
-	for (int i = start; i < stop ; i++) {
-		sum += 1.0/log(i);
-	}
+    if(start < 2 || stop <= start){
+        printf("Start must be greater than 2 and the end must be larger than start.\n");
+        exit(1);
+    }
 
-	// Debug prints if needed
+    MPI_Init(NULL, NULL);
+    //MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);/* get current process id */
+    MPI_Comm_size(MPI_COMM_WORLD, &size);/* get number of processsses */
 
-	// TODO: Get the sum of each node's partial answer
-	// Use MPI_Send and MPI_Recv here
+    printf("Number of process: %d\n", size);
 
-	// TODO: Print the global sum once only
-	printf("The sum is: %f\n", sum);
+    // TODO: Compute the local range, so that all the elements are accounted for.
+    double local_range = (stop - start) / size;
 
-	return 0;
+    double total_sum = 0.0;
+    double local_sum = 0.0;
+    double rest = 0.0;
+
+    if (rank == size - 1) {
+        rest = (stop - start) % size;
+    } else {
+        rest = 0;
+    }
+
+    int local_start = start+rank*local_range;
+    int local_stop = start+(rank+1)*local_range+rest;
+
+    printf("%d %d\n", local_start, local_stop);
+
+    printf("Running calculate on %d to %d\n", start, stop);
+    for (int i = start+rank-1; i < stop; i += size) {
+        printf("index: %d\n", i);
+        local_sum += 1.0/log(i);
+        printf("calculated: %f\n", local_sum);
+    }
+
+    // Perform the computation
+    if (rank == 0) {
+        double recv_sum = 0.0;
+        for (int worker = 1; worker < size; worker++) {
+            MPI_Recv(&recv_sum, 1, MPI_DOUBLE, worker, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            total_sum += recv_sum;
+        }
+        printf("The sum is: %f\n", total_sum);
+    } else {
+        MPI_Send(&local_sum, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+    }
+
+    MPI_Finalize();
+
+
+    return 0;
 }
 
+double calculate_interval(int start, int stop) {
+    double sum = 0.0;
+    printf("Running calculate on %d to %d\n", start, stop);
+    for (int i = start; i < stop ; i++) {
+        sum += 1.0/log(i);
+    }
+    return sum;
+}
