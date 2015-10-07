1e8e2c6df35472e4b14372888b4fea66931e0630
Kristoffer Dalby <kradalby@kradalby.no>
2015-10-07 21:35:50 +0200

    added stuff

--- tdt4225/e2/bench.py
+++ tdt4225/e2/bench.py
@@ -1,6 +1,6 @@
-from datetime import datetime, timedelta
 import os
 import array
+import time
 
 
 BLOCKSIZE = 8192
@@ -10,21 +10,26 @@ BLOCK = array.array('b', [1 for x in range(8192 - 64)])
 def pretty(ms, size):
     megabyte = size * 1024
     throughput = megabyte / (ms / 1000)
+    throughput = '{0:.2f}'.format(throughput)
+    ms = '{0:.2f}'.format(ms)
     print('{} GB\t{} MB/s\t{} ms'.format(size, throughput, ms))
 
 print('{}\t{}\t{}'.format('EXT4', 'Throughput', 'Time'))
-for size in [1, 2, 4]:
-#for size in [1, 2, 4, 8, 16, 32]:
-    with open('test', 'wb') as f:
-        start = datetime.now()
+#for size in [1, 2, 4]:
+for size in [1, 2, 4, 8, 16, 32]:
+    with open('test{}'.format(size), 'wb') as f:
+        start = time.clock()
 
-        for i in range(NBLOCKS):
+        for i in range(NBLOCKS * size):
             f.write(BLOCK)
 
-        stop = datetime.now()
+        f.flush()
+        os.system('sync')
+        #os.fsync(f)
+        stop = time.clock()
 
         interval = stop - start
 
-        pretty(interval.microseconds/1000, size)
+        pretty(interval*1000, size)
 
-    os.remove('test')
+    #os.remove('test')
