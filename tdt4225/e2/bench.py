12b6be2f094d7a59823fc3773654379bcedb296c
Kristoffer Dalby <kradalby@kradalby.no>
2015-10-07 21:51:24 +0200

    added comments

--- tdt4225/e2/bench.py
+++ tdt4225/e2/bench.py
@@ -5,9 +5,14 @@ import time
 
 BLOCKSIZE = 8192
 NBLOCKS = 131072
-BLOCK = array.array('b', [1 for x in range(8192 - 64)])
+
+# Define a array of 8192 bits
+BLOCK = array.array('b', [1 for x in range(BLOCKSIZE - 64)])
 
 def pretty(ms, size):
+    '''
+    This method pretty prints the output
+    '''
     megabyte = size * 1024
     throughput = megabyte / (ms / 1000)
     throughput = '{0:.2f}'.format(throughput)
@@ -15,9 +20,11 @@ def pretty(ms, size):
     print('{} GB\t{} MB/s\t{} ms'.format(size, throughput, ms))
 
 print('{}\t{}\t{}'.format('EXT4', 'Throughput', 'Time'))
-#for size in [1, 2, 4]:
+
 for size in [1, 2, 4, 8, 16, 32]:
+    # Open the file in binary form
     with open('test{}'.format(size), 'wb') as f:
+
         start = time.clock()
 
         for i in range(NBLOCKS * size):
@@ -31,5 +38,3 @@ for size in [1, 2, 4, 8, 16, 32]:
         interval = stop - start
 
         pretty(interval*1000, size)
-
-    #os.remove('test')
